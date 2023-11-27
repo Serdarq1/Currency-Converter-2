4e9196181f5b9580f9157589ebd6beb64fe84259
serdarakova <67336836+Serdarq1@users.noreply.github.com>
2023-11-27 16:20:10 +0300

    Add files via upload

---
+++ currency converter/script.js
@@ -0,0 +1,224 @@
+currencies = [
+    "AED",
+    "AFN",
+    "ALL",
+    "AMD",
+    "ANG",
+    "AOA",
+    "ARS",
+    "AUD",
+    "AWG",
+    "AZN",
+    "BAM",
+    "BBD",
+    "BDT",
+    "BGN",
+    "BHD",
+    "BIF",
+    "BMD",
+    "BND",
+    "BOB",
+    "BRL",
+    "BSD",
+    "BTC",
+    "BTN",
+    "BWP",
+    "BYN",
+    "BZD",
+    "CAD",
+    "CDF",
+    "CHF",
+    "CLF",
+    "CLP",
+    "CNY",
+    "COP",
+    "CRC",
+    "CUC",
+    "CUP",
+    "CVE",
+    "CZK",
+    "DJF",
+    "DKK",
+    "DOP",
+    "DZD",
+    "EGP",
+    "ERN",
+    "ETB",
+    "EUR",
+    "FJD",
+    "FKP",
+    "GBP",
+    "GEL",
+    "GGP",
+    "GHS",
+    "GIP",
+    "GMD",
+    "GNF",
+    "GTQ",
+    "GYD",
+    "HKD",
+    "HNL",
+    "HRK",
+    "HTG",
+    "HUF",
+    "IDR",
+    "ILS",
+    "IMP",
+    "INR",
+    "IQD",
+    "IRR",
+    "ISK",
+    "JEP",
+    "JMD",
+    "JOD",
+    "JPY",
+    "KES",
+    "KGS",
+    "KHR",
+    "KMF",
+    "KPW",
+    "KRW",
+    "KWD",
+    "KYD",
+    "KZT",
+    "LAK",
+    "LBP",
+    "LKR",
+    "LRD",
+    "LSL",
+    "LYD",
+    "MAD",
+    "MDL",
+    "MGA",
+    "MKD",
+    "MMK",
+    "MNT",
+    "MOP",
+    "MRO",
+    "MRU",
+    "MUR",
+    "MVR",
+    "MWK",
+    "MXN",
+    "MYR",
+    "MZN",
+    "NAD",
+    "NGN",
+    "NIO",
+    "NOK",
+    "NPR",
+    "NZD",
+    "OMR",
+    "PAB",
+    "PEN",
+    "PGK",
+    "PHP",
+    "PKR",
+    "PLN",
+    "PYG",
+    "QAR",
+    "RON",
+    "RSD",
+    "RUB",
+    "RWF",
+    "SAR",
+    "SBD",
+    "SCR",
+    "SDG",
+    "SEK",
+    "SGD",
+    "SHP",
+    "SLL",
+    "SOS",
+    "SRD",
+    "SSP",
+    "STD",
+    "STN",
+    "SVC",
+    "SYP",
+    "SZL",
+    "THB",
+    "TJS",
+    "TMT",
+    "TND",
+    "TOP",
+    "TRY",
+    "TTD",
+    "TWD",
+    "TZS",
+    "UAH",
+    "UGX",
+    "USD",
+    "UYU",
+    "UZS",
+    "VEF",
+    "VES",
+    "VND",
+    "VUV",
+    "WST",
+    "XAF",
+    "XAG",
+    "XAU",
+    "XCD",
+    "XDR",
+    "XOF",
+    "XPD",
+    "XPF",
+    "XPT",
+    "YER",
+    "ZAR",
+    "ZMW",
+    "ZWL",
+  ];
+
+let apiKey = "0af7ceff20ce2b15d86181e9"
+let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`
+
+const fromDropdown = document.getElementById("from-currency-select")
+const toDropDown = document.getElementById("to-currency-select")
+const btn = document.getElementById("convert-button")
+const result = document.getElementById("result")
+
+
+currencies.forEach((currency) =>{
+    const option = document.createElement("option")
+    option.value = currency
+    option.text = currency
+    fromDropdown.add(option)
+})
+
+currencies.forEach((currency) =>{
+    const option = document.createElement("option")
+    option.value = currency
+    option.text = currency
+    toDropDown.add(option)
+})
+
+fromDropdown.value = "USD"
+toDropDown.value = "TRY"
+
+let convertCurrency = () => {
+    const amount = document.querySelector("#amount").value
+    const fromCurrency = fromDropdown.value
+    const toCurrency = toDropDown.value
+
+    if(amount < 1){
+        result.innerHTML = "Amount can't be less than 1."
+        console.log("o")
+    }
+
+        if(amount > 0){
+            fetch(api).then(res => res.json()).then(data => {
+                let fromExchangeRate = data.conversion_rates[fromCurrency]
+                let toExchangeRate = data.conversion_rates[toCurrency]
+
+                const convertedAmount = (amount / fromExchangeRate) * toExchangeRate
+                result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`
+            })
+        }else{
+            alert("Please write a proper amount.")
+        }
+}
+
+
+btn.addEventListener("click", convertCurrency)
